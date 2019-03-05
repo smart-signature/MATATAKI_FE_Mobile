328421922b70f80c975e007823add1c1b47c3797
Frank Wei <frank@frankwei.xyz>
2019-03-05 17:26:19 +0800

    汉化 router 关于 code-splitting 的注释

--- src/router.js
+++ src/router.js
@@ -16,9 +16,9 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      // 路由级 code-splitting
+      // 这会给当前的路由页生成单独的块文件 (webpackChunkName 是 about 则得到 about.[版本哈希].js) 
+      // 只有使用该 route 的场合才会下载这个页面的代码 (惰性加载).
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
   ],
