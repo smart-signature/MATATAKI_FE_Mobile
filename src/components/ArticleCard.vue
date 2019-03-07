35eb00a653b7dd43fa1cf683675f54d3c1c4a73d
Frank Wei <frank@frankwei.xyz>
2019-03-07 15:08:20 +0800

    ArticleCard now display relative time

--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -2,15 +2,21 @@
   <div class="card article">
     <a :href="article.articleUrl">
       <h2 class="title">{{article.title}}</h2>
-      <p>by {{article.author}}</p>
+      <p class="article-infomation">{{friendlyDate}} by {{article.author}}</p>
     </a>
   </div>
 </template>
 
 <script>
+import moment from "moment";
 export default {
   name: "ArticleCard",
-  props: ["article"]
+  props: ["article"],
+  computed: {
+    friendlyDate() {
+      return moment(this.article.time).fromNow();
+    }
+  }
 };
 </script>
 
@@ -18,5 +24,14 @@ export default {
 a {
   color: #000;
   text-decoration: none; /* no underline */
+}
+h2.title {
+  font-size: 18px;
+  font-weight: 600;
+  color: rgb(76,76,76);
+  /* line-height: 25px; */
+}
+.article-infomation {
+  color: rgb(105,105,105);
 }
 </style>
