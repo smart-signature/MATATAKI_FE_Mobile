7d8b6117cff89215583284a6e477d546a1502715
咚咚 <lf2kexe@gmail.com>
2019-03-08 17:23:03 +0800

    Add WangEditor

---
+++ src/components/WangEditor.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="WangEditor" >
+        <div ref="editor" style="text-align:left"></div>
+        <button v-on:click="getContent">查看内容</button>
+    </div>
+</template>
+
+<script>
+    import E from 'wangeditor'
+    export default {
+      name: 'WangEditor',
+      data () {
+        return {
+          editorContent: ''
+        }
+      },
+      methods: {
+        getContent: function () {
+            alert(this.editorContent)
+        }
+      },
+      mounted() {
+        var editor = new E(this.$refs.editor)
+        editor.customConfig.onchange = (html) => {
+          this.editorContent = html
+        }
+        editor.create()
+      }
+    }
+</script>
+
+<style scoped>
+.WangEditor {
+        margin: auto;
+        width: 100%;
+        height: 580px;
+}
+</style>
