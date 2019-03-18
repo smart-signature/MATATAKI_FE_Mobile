21434ad3c355ad689b3c7d7ec7b3c4a1f51e144a
Frank Wei <frank@frankwei.xyz>
2019-03-18 23:57:21 +0800

    Merge pull request #55 from Deasoso/testing
    user page.

--- src/views/User.vue
+++ src/views/User.vue
@@ -14,18 +14,12 @@
         <p class="username">{{username}}</p>
         <p class="userstatu">关注：13 粉丝：20.8w</p>
       </div>
-
       <div v-if="editing">
         <Button class="rightbutton" size="small" type="success"
-                ghost @click="cancel">
-          <div>取消</div>
-        </Button>
-        <Button class="rightbutton" size="small" type="success"
                 ghost @click="save">
-          <div>保存</div>
+          <div>完成</div>
         </Button>
       </div>
-
       <div v-else>
         <Button v-if="isMe"
                 class="rightbutton" size="small" type="success" ghost @click="edit">
@@ -36,37 +30,62 @@
           <div>关注</div>
         </Button>
       </div>
-
     </div>
-    <div class="centercard" v-if="isMe">
+    <div class="topcard" v-if="isMe">
       <Row type="flex" justify="center" class="code-row-bg">
-          <Col span="5">
-            <router-link :to="{ name: 'Asset', params: { username }}">
-              <!-- TODO(minakokojima): 'sign_income' of null
-              <p class="centervalue">{{playerincome.sign_income/1000}}</p>
-              -->
-              <p class="centervalue">{{0}}
-              <p class="centertext">支持收入</p>
-            </router-link>
+          <Col span="11">
+            <!-- TODO(minakokojima): 'sign_income' of null
+            <p class="centervalue">{{playerincome.sign_income/1000}}</p>
+            -->
+            <p class="centervalue">{{0}}</p>
+            <p class="centertext">支持收入</p>
           </Col>
-          <Col span="1"><Divider type="vertical" /></Col>
-          <Col span="5">
-            <router-link :to="{ name: 'Asset', params: { username }}">
-              <p class="centervalue">{{playerincome.share_income/1000}}</p>
-              <p class="centertext">转发收入</p>
-            </router-link>
+          <Col span="1"><Divider type="vertical" style="height:33px;margin-top:10px;" /></Col>
+          <Col span="11">
+            <p class="centervalue">{{playerincome.share_income/1000}}</p>
+            <p class="centertext">转发收入</p>
           </Col>
       </Row>
     </div>
-    <!-- todo(minakokojima): 顯示該作者發表的文章。 !-->
-    <ArticlesList ref="ArticlesList"/>
+    <!-- todo(minakokojima): 顯示該作者發表的文章。-->
+    <!-- <ArticlesList ref="ArticlesList"/> -->
+    <div class="centercard" v-if="isMe">
+      <za-cell is-link has-arrow @click='() => {}'>
+        <router-link :to="{ name: 'Asset', params: { username }}">
+          资产明细
+        </router-link>
+        <!-- <za-icon type='right' slot='icon'/> -->
+      </za-cell>
+      <za-cell is-link has-arrow @click='() => {}'>
+        原创文章
+      </za-cell>
+      <za-cell is-link has-arrow @click='() => {}'>
+        打赏文章
+      </za-cell>
+    </div>
+    <div class="centercard" v-if="isMe">
+      <za-cell is-link has-arrow @click='() => {}'>
+        规则介绍
+      </za-cell>
+      <za-cell is-link has-arrow @click='() => {}'>
+        用户协议
+      </za-cell>
+      <za-cell is-link has-arrow @click='() => {}'>
+        隐私政策
+      </za-cell>
+    </div>
+    <div class="bottomcard" v-if="isMe">
+      <Button class="bottombutton" long>退出登录</Button>
+    </div>
+    <ArticlesList :listtype="'others'" ref='ArticlesList' v-if="!isMe"/>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
 import { getPlayerIncome } from '../api/signature';
-import ArticlesList from './Home/ArticlesList.vue';
+import ArticlesList from './User/ArticlesList.vue';
+import API from '../api/scatter.js';
 
 export default {
   name: 'User',
@@ -126,11 +145,16 @@ a {
 }
 .user{
   background-color: #F7F7F7;
+  padding-bottom: 20px;
 }
 .usercard{
+  box-shadow: 0px 2px 5px 3px rgba(233, 233, 233, 0.5);
+  background-color: #ffffff;
   padding: 20px;
-  display: inline-block;
-  width: 100%;
+  margin: 20px;
+  /* display: inline-block; */
+  border-radius: 8px;
+  height: 99px;
 }
 .userpic{
   float: left;
@@ -145,23 +169,45 @@ a {
 }
 .texts{
   float: left;
-  padding-left: 15px;
+  padding-left: 11px;
 }
 .rightbutton{
   float: right;
-  width: 44px;
+  margin-top: 6px;
+  width: 62px;
   height: 19px;
   vertical-align: middle;
+}
+.topcard{
+  background-color: #ffffff;
+  margin-left: 20px;
+  margin-right: 20px;
+  margin-bottom: 20px;
+  padding: 10px;
+  background: rgba(255, 255, 255, 1);
+  box-shadow: 0px 2px 5px 3px rgba(233, 233, 233, 0.5);
+  border-radius: 8px;
 }
 .centercard{
   background-color: #ffffff;
   margin-left: 20px;
   margin-right: 20px;
-  padding: 10px;
-  float: center;
+  margin-bottom: 20px;
   background: rgba(255, 255, 255, 1);
   box-shadow: 0px 2px 5px 3px rgba(233, 233, 233, 0.5);
   border-radius: 8px;
+}
+.bottomcard{
+  background-color: #ffffff;
+  margin-left: 20px;
+  margin-right: 20px;
+  background: rgba(255, 255, 255, 1);
+  box-shadow: 0px 2px 5px 3px rgba(233, 233, 233, 0.5);
+  border-radius: 8px;
+}
+.bottombutton{
+  height: 55px;
+  font-size: 14px;
 }
 .centervalue{
   font-size: 22px;
