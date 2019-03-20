2d15bcc69c66cb846cd5e9be730809ecebad05a3
咚咚 <lf2kexe@gmail.com>
2019-03-21 04:32:13 +0800

    Add Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="my-Header">      
+    <za-nav-bar>
+      <div slot="left"><Icon type="ios-home" :size="24" @click="goHome" /></div>
+      <div slot="title" @click="goHome">{{pageinfo.title}}</div>
+      <div slot="right"><Icon type="ios-share-alt" :size="24" @click="share" /></div>
+    </za-nav-bar>
+</div>
+</template>
+
+<script>
+import { mapState, mapActions, mapGetters } from 'vuex';
+
+export default {
+  name: 'my-Header',
+  props: ['pageinfo'],
+  computed: {
+  },
+  created() {
+    try { // setup
+      this.suggestNetworkAsync();
+    } catch (error) {
+      console.error('suggestNetwork error');
+    }
+    try {
+      this.connectScatterAsync().then(connected => {
+        if(!connected) return console.error('no scatter');
+        this.loginScatterAsync();
+      });
+    } catch (error) {
+      console.error('login faild');
+      this.$Modal.error({
+        title: '无法与你的钱包建立链接',
+        content: '请检查钱包是否打开并解锁',
+      });
+    }
+  },
+  methods: {
+    ...mapActions([
+      'connectScatterAsync',
+      'suggestNetworkAsync',
+      'loginScatterAsync',
+    ]),
+    goHome() {
+      this.$router.push({ name: 'home' });
+    },
+
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
