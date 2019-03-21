e9eae625711e3eb42601374a185e5a8533805ddd
咚咚 <lf2kexe@gmail.com>
2019-03-21 23:16:22 +0800

    Add other network

--- src/config/network.js
+++ src/config/network.js
@@ -6,13 +6,22 @@ export const networks = {
   //   port: 443,
   //   chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
   // },
-  eos: {
-    protocol: 'https',
-    blockchain: 'eos',
-    host: 'nodes.get-scatter.com',
-    port: 443,
-    chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-  },
+  eos: [
+      {
+        protocol: 'https',
+        blockchain: 'eos',
+        host: 'nodes.get-scatter.com',
+        port: 443,
+        chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+      },
+      {
+        protocol: 'https',
+        blockchain: 'eos',
+        host: 'api.eosnewyork.io',
+        port: 443,
+        chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+      }
+  ],
   bos: {
     protocol: 'https',
     blockchain: 'eos',
