40e534c7a7e6b93d8bc8ba46630adc6fed3f40f0
xiaotiandada <952822399@qq.com>
2019-03-30 16:59:29 +0800

    替换alert、优化提示
    替换alert(alert 会阻塞进程)、优化提示 无论成功还是失败都会提示并且关闭弹出框

--- src/views/User/Asset.vue
+++ src/views/User/Asset.vue
@@ -214,7 +214,13 @@ export default {
     },
     async withdraw(name) {
       console.log('Connecting to EOS fetch data...');
-      await withdraw(name).then(() => alert('提现成功!'));
+      await withdraw(name)
+        .then(() => this.$Message.success('提现成功!'))
+        .catch((err) => {
+          console.log(err);
+          this.$Message.error('提现失败!');
+        });
+      this.visible = false; // 成功和失败都关闭弹窗
     },
   },
 };
