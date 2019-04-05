3623a1547111ebb3ec2b52fd7cbcac6fd5b528c1
咚咚 <lf2kexe@gmail.com>
2019-04-05 19:18:29 +0800

    Remove some blablabla

--- src/store.js
+++ src/store.js
@@ -70,19 +70,6 @@ export default new Vuex.Store({
     async suggestNetworkAsync() {
       await api.suggestNetworkAsync();
     },
-    // 這不該是 async
-    // 理由:只有一個 await api.loginScatterAsync() ，
-    // 那直接讓他接.catch 彈框表示'无法与你的钱包建立链接'就好
-    // 而外層使用的 loginScatterAsync 的場景並沒有需要 .then .catch
-    // 而是都是登錄與操作分開的場景，最需要的是自動登陸，而不是按下按鈕在執行前的登陸
-    // 而且此 async 並沒有任何 return ，
-    // 唯一依靠是內層 await api.loginScatterAsync() return 的 Promise
-    // 又被 try catch 接走，等於此 func 外頭沒有接到任何 return 的 Promise
-    // =====
-    // 又由於引用此 func 的數量過多，重構不太好
-    // 因此會新分出一個新的非 async 的 loginScatter 先做使用於新更動的地方
-    // 再逐一去除 loginScatterAsync 的依賴，或是整體調用 loginScatter 的位置重配優先
-    // 以上 todo
     // 參考 https://es6.ruanyifeng.com/#docs/async
     async loginScatterAsync({ commit, dispatch }) {
       console.log('Start log in...');
