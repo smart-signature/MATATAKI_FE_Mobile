690baa6a6540c0d2f07b6edd479dff8ebb68ab6e
Frank Wei <frank@frankwei.xyz>
2019-04-08 20:46:58 +0800

    feat: easter eggs redirect to hidden page

---
+++ src/views/EasterEgg.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="easter-egg">
+        <h1 class="title">🎉 恭喜你发现了隐藏的彩蛋！</h1>
+        <p>当前模式：{{env}}</p>
+        <div class="dev-only" v-if="isDevelopment">
+            <h1 class="title">Development 专有彩蛋</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Easter-Egg",
+    computed: {
+        env() {
+            return process.env.NODE_ENV;
+        },
+        isDevelopment() {
+            return this.env === "development";
+        }
+    }
+}
+</script>
