7a7325c05436507c7af60f695c235aa5ea7b5b51
lychees <lychees67@gmail.com>
2019-04-13 08:25:10 +0800

    Add AvatarUploader via vue-upload-component
    https://github.com/lian-yue/vue-upload-component

---
+++ src/views/User/AvatarUploader.vue
@@ -0,0 +1,13 @@
+<template>
+  <img style="position:absolute; z-index:1;left:40px;"
+          width="50px" src="/img/camera.png">
+</template>
+
+<script>
+import { OrderBy, getArticles } from '@/api/backend';
+import { ArticleCard } from '@/components/';
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'AvatarUploader',
+}
