381f39f06aee49132ef80e87766b362104ba3179
xiaotiandada <952822399@qq.com>
2019-04-19 14:08:39 +0800

    资产明细分页替换
    资产明细分页替换

--- src/components/PullComponents.vue
+++ src/components/PullComponents.vue
@@ -45,6 +45,14 @@ export default {
       type: Number,
       default: 0,
     },
+    // 返回的数据是对象还是数组
+    isObj: {
+      type: Object,
+      default: () => ({
+        type: 'Array',
+        key: '',
+      }),
+    },
   },
   computed: {
     displayAboutScroll() {
@@ -66,20 +74,28 @@ export default {
       const params = this.params || {};
       params.page = this.page;
       axios.get(`${apiServer}/${this.apiUrl}`, { params }).then(({ data }) => {
-        // 清空数组 ps: 如果在 refresh 里面清空数组
-        // 1.点击的时会先执行触摸刷新的方法 导致无法正常单击切换页面
-        // 2.因为先执行触摸方法 清空了数组 会给页面造成影响
         if (isEmptyArray) this.articles.length = 0;
-        // Merge arrays with destruction
-        this.articles = [...this.articles, ...data];
-        this.$emit('getListData', {
-          data: this.articles,
-          index: this.nowIndex,
-        });
-        if (data.length >= 0 && data.length < 20) this.isTheEndOfTheScroll = true;
-        else this.page += 1;
+        if (this.isObj.type === 'Array') {
+          // 如果返回的数据是 Array 返回整个 data
+          this.articles = [...this.articles, ...data];
+          this.$emit('getListData', {
+            data: this.articles,
+            index: this.nowIndex,
+          });
+          if (data.length >= 0 && data.length < 20) this.isTheEndOfTheScroll = true;
+        } else if (this.isObj.type === 'Object') {
+          // 如果返回的是 Object 根据传进来的字段获取相应的 list
+          const resData = data[this.isObj.key];
+          this.articles = [...this.articles, ...resData];
+          this.$emit('getListData', {
+            data,
+            list: this.articles,
+            index: this.nowIndex,
+          });
+          if (resData.length >= 0 && resData.length < 20) this.isTheEndOfTheScroll = true;
+        }
+        this.page += 1;
         this.busy = false;
-        // 列表最后一列小于二十显示加载完
       }).catch((err) => {
         console.log(err);
         this.$Message.error('获取文章发生错误');
@@ -112,8 +128,9 @@ export default {
 <style scoped>
 /* 加载更多提示 */
 .loading-stat {
-  margin: 10px;
+  margin: 20px 0;
   color: #999;
-  font-size: 13px;
+  font-size: 14px;
+  text-align: center;
 }
 </style>
---
+++ src/views/User/Asset/Asset.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="assetpage">
+    <BaseHeader
+      :pageinfo="{ left: 'back', title: `${newName.length >= 12 ?  `${newName.substring(0,12)}...` : newName}的资产明细`, rightPage: 'home',
+                   needLogin: false, }"/>
+    <div class="topcard">
+      <div class="toptext1">待提现</div><br/>
+      <div class="topremain">{{playerincome.toFixed(4)}}</div>
+      <div style="position:absolute;right:40px;top:90px;">
+          <Button class="withdraw" ghost @click="visible = true"><div style="margin-top:-2px">提现</div></Button>
+          <za-confirm
+            :visible="visible"
+            title="提现确认" message="确定要提现吗？"
+            :ok="handleOk" :cancel="() => { visible = false }"></za-confirm>
+      </div>
+      <Divider style="margin-top:10px;margin-bottom:10px;"/>
+      <Row type="flex" justify="center" class="code-row-bg">
+        <Col span="6">
+          <p class="toptext2">创作历史收益</p>
+          <p class="toptext3"
+            :style='assetsRewards.totalSignIncome > 0 ? { color: "#f50" }
+                    : (assetsRewards.totalSignIncome < 0 ? { color: "#87d068" } : {color: "#a7aab7"})'>
+            {{assetsRewards.totalSignIncome}}
+          </p>
+        </Col>
+        <Col span="3" style="text-align:center">
+          <Divider type="vertical" style="height:20px;margin-top:16px;" />
+        </Col>
+        <Col span="6">
+          <p class="toptext2">赞赏历史收益</p>
+          <p class="toptext3"
+             :style='assetsRewards.totalShareIncome > 0 ? { color: "#f50" }
+                     : (assetsRewards.totalShareIncome < 0 ? { color: "#87d068" } : {color: "#a7aab7"})'>
+            {{assetsRewards.totalShareIncome}}
+          </p>
+        </Col>
+        <Col span="3" style="text-align:center">
+          <Divider type="vertical" style="height:20px;margin-top:16px;" />
+        </Col>
+        <Col span="6">
+          <p class="toptext2">赞赏历史支出</p>
+          <p class="toptext3"
+            :style='assetsRewards.totalShareExpenses > 0 ? { color: "#f50" }
+                    : (assetsRewards.totalShareExpenses < 0 ? { color: "#87d068" } : {color: "#a7aab7"})'>
+            {{assetsRewards.totalShareExpenses}}
+          </p>
+        </Col>
+      </Row>
+    </div>
+    <div class="detailtext">明细</div>
+    <AssetList :username="username" @getOtherAsset="getOtherAsset" />
+  </div>
+</template>
+
+<script>
+import { getUser } from '@/api';
+import { getPlayerIncome, withdraw } from '@/api/signature';
+import { isEmptyArray } from '@/common/methods';
+import AssetList from './AssetList.vue';
+
+export default {
+  name: 'Asset',
+  props: ['username'],
+  components: { AssetList },
+  created() {
+    this.getPlayerTotalIncome(this.username);
+    this.getUser(this.username);
+  },
+  data() {
+    return {
+      assets: [],
+      playerincome: 0,
+      assetsRewards: {
+        totalSignIncome: 0,
+        totalShareIncome: 0,
+        totalShareExpenses: 0,
+      },
+      visible: false,
+      newName: '',
+    };
+  },
+  computed: { },
+  methods: {
+    async getPlayerTotalIncome(name) {
+      console.log('Connecting to EOS fetch player income...');
+      const playerincome = await getPlayerIncome(name); // 从合约拿到支持收入和转发收入
+      this.playerincome = isEmptyArray(playerincome)
+        ? (playerincome[0].share_income + playerincome[0].sign_income) / 10000
+        : 0;
+      // 截止2019年3月24日中午12时合约拿过来的东西要除以10000才能正常显示
+    },
+    handleOk() {
+      this.withdraw(this.username);
+    },
+    async withdraw(name) {
+      console.log('Connecting to EOS fetch data...');
+      await withdraw(name)
+        .then(() => this.$Message.success('提现成功!'))
+        .catch((err) => {
+          console.log(err);
+          this.$Message.error('提现失败!');
+        });
+      this.visible = false; // 成功和失败都关闭弹窗
+    },
+    async getUser(username) {
+      await getUser({ username }).then(({ data }) => {
+        this.newName = data.nickname || data.username;
+      }).catch((err) => {
+        this.newName = this.username;
+        console.log(err);
+      });
+    },
+    getOtherAsset(res) {
+      // console.log(res);
+      const {
+        totalSignIncome: sign,
+        totalShareIncome: shareIn,
+        totalShareExpenses: shareExp,
+      } = res.data;
+      const { assetsRewards } = this;
+      assetsRewards.totalSignIncome = sign > 0 ? `+${sign / 10000}` : sign / 10000;
+      assetsRewards.totalShareIncome = shareIn > 0 ? `+${shareIn / 10000}` : shareIn / 10000;
+      assetsRewards.totalShareExpenses = shareExp > 0 ? `+${shareExp / 10000}` : shareExp / 10000;
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped src="./Asset.less"></style>
