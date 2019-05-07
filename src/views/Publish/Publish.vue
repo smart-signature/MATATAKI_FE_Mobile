1e7e51084fd0d43acb579e64699e20abfd347ede
xiaotiandada <952822399@qq.com>
2019-05-07 17:25:14 +0800

    markdown 编辑器配置
    markdown 编辑器配置

--- src/views/Publish/Publish.vue
+++ src/views/Publish/Publish.vue
@@ -55,6 +55,7 @@ import VueSlider from 'vue-slider-component';
 import 'vue-slider-component/theme/default.css';
 import ImgUpload from '@/components/ImgUpload';
 import { sleep } from '@/common/methods';
+import { toolbars } from './toolbars'; // 编辑器配置
 
 export default {
   name: 'NewPost',
@@ -324,34 +325,9 @@ export default {
     },
     setToolBar(val) {
       if (val > 440) {
-        this.toolbars = {
-          bold: true, // 粗体
-          italic: true, // 斜体
-          header: true, // 标题
-          underline: true, // 下划线
-          strikethrough: true, // 中划线
-          quote: true, // 引用
-          ol: true, // 有序列表
-          ul: true, // 无序列表
-          link: true, // 链接
-          imagelink: true, // 图片链接
-          code: true, // code
-          table: true, // 表格
-          htmlcode: true, // 展示html源码
-          help: true, // 帮助
-          preview: true, // 预览
-          subfield: true,
-          navigation: true,
-        };
+        this.toolbars = toolbars.pc;
       } else {
-        this.toolbars = {
-          bold: true, // 粗体
-          italic: true, // 斜体
-          header: true, // 标题
-          imagelink: true, // 图片链接
-          preview: true, // 预览
-          help: true,
-        };
+        this.toolbars = toolbars.mobile;
       }
     },
     resize() {
---
+++ src/views/Publish/toolbars.js
@@ -0,0 +1,30 @@
+// eslint-disable-next-line import/prefer-default-export
+export const toolbars = {
+  pc: {
+    bold: true, // 粗体
+    italic: true, // 斜体
+    header: true, // 标题
+    underline: true, // 下划线
+    strikethrough: true, // 中划线
+    quote: true, // 引用
+    ol: true, // 有序列表
+    ul: true, // 无序列表
+    link: true, // 链接
+    imagelink: true, // 图片链接
+    code: true, // code
+    table: true, // 表格
+    htmlcode: true, // 展示html源码
+    help: true, // 帮助
+    preview: true, // 预览
+    subfield: true,
+    navigation: true,
+  },
+  mobile: {
+    bold: true, // 粗体
+    italic: true, // 斜体
+    header: true, // 标题
+    imagelink: true, // 图片链接
+    preview: true, // 预览
+    help: true,
+  },
+};
