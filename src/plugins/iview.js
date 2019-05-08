831c83f34a7fa4751e86a30848a24946c36b24cf
咚咚 <lf2kexe@gmail.com>
2019-05-08 22:22:53 +0800

    Add modularly import plugin for iview

--- src/plugins/iview.js
+++ src/plugins/iview.js
@@ -1,6 +1,18 @@
 import Vue from 'vue';
-import iView from 'iview';
+import { 
+  BackTop, Button, Icon, Row, Divider, message, Notice, Modal, Avatar, Tooltip
+} from 'iview';
 
 import 'iview/dist/styles/iview.css';
 
-Vue.use(iView);
+// Vue.use(iView);
+Vue.component('BackTop', BackTop);
+Vue.component('Button', Button);
+Vue.component('Icon', Icon);
+Vue.component('Row', Row);
+Vue.component('Divider', Divider);
+Vue.component('message', message);
+Vue.component('Notice', Notice);
+Vue.component('Modal', Modal);
+Vue.component('Avatar', Avatar);
+Vue.component('Tooltip', Tooltip);
