ec5b9c6d2ca2452c9727740581c33da74940bdc1
xiaotiandada <952822399@qq.com>
2019-05-17 18:08:34 +0800

    update code
    update code

--- src/common/reg.js
+++ src/common/reg.js
@@ -3,7 +3,4 @@
  * @param {地址} address 判断地址 是EOS还是ONT
  */
 // eslint-disable-next-line import/prefer-default-export
-export const isAddress = (address) => {
-  if (/^A[0-9a-zA-Z]{33}$/.test(address)) return true;
-  return false;
-};
+export const isAddress = address => (/^A[0-9a-zA-Z]{33}$/.test(address));
