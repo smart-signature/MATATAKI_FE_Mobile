0def0cfbb50a12a11a44870f74d195cc0803a03b
xiaotiandada <952822399@qq.com>
2019-05-30 18:50:48 +0800

    fix xss

--- src/common/xss.js
+++ src/common/xss.js
@@ -3,11 +3,12 @@ import xss from 'xss';
 // eslint-disable-next-line import/prefer-default-export
 export const xssFilter = (html) => {
   // 自定义规则
+  const { whiteList } = xss;
+  whiteList.iframe = ['src', 'height', 'width', 'frameborder', 'allowfullscreen'];
+  whiteList.embed = ['src', 'allowFullScreen', 'quality', 'width', 'height', 'align', 'type'];
+
   const options = {
-    whiteList: {
-      iframe: ['src', 'height', 'width', 'frameborder', 'allowfullscreen'],
-      embed: ['src', 'allowFullScreen', 'quality', 'width', 'height', 'align', 'type'],
-    },
+    whiteList,
   };
   const myxss = new xss.FilterXSS(options);
   return myxss.process(html);
