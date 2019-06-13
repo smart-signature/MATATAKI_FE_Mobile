cffda7027bb10952c78a146daa7cc372f4c6a2b9
xiaotiandada <952822399@qq.com>
2019-06-13 13:16:21 +0800

    tag color

--- src/common/tagColor.js
+++ src/common/tagColor.js
@@ -1,13 +1,10 @@
 export default () => {
   // tag id 对应相应的颜色, 全局统一
-  const id = 12
-  const color = [
-    '#1c9cfe', '#fb6877', '#886cff', '#ffa443', '#fb6877', '#fdd55c', '#83dbff', '#9ce883', '#ffd7b6',
-    '#1c9cfe', '#fb6877', '#886cff',
-  ]
+  const id = 18
+  const color = [ '#1C9CFE', '#FB6877', '#886CFF', '#FFA443', '#44D7B6', '#FDD55C', '#83DBFF', '#9CE883', '#FFD7B6']
   let tag = {}
 
   for (let i = 1; i <= id; i++)
-    tag[i] = color[i-1]
+    tag[i] = color[(i - 1) % color.length]
   return tag
 }
