ab12923bceb20ba593a658492599d713c6263f8c
xiaotiandada <952822399@qq.com>
2019-06-14 21:11:29 +0800

    配置修改
    配置修改

--- public/widget/index.js
+++ public/widget/index.js
@@ -1,55 +1,59 @@
-(function(){
-    // 获取url信息
-    const urlSearch = window.location.search.substr(1).split('&')
-    // 获取容器
-    const appDom = document.querySelector('#app')
-    // 跳转地址
-    let url = `https://test.smartsignature.io`
-    // 接口地址
-    let baseUrl = `https://apitest.smartsignature.io`
-    // 默认图片 logo
-    let logoImg = '../img/logo.png'
-    // 数据
-    let urlSearchData = {}
+/* eslint-disable func-names */
+(function () {
+  // 获取url信息
+  const urlSearch = window.location.search.substr(1).split('&');
+  // 获取容器
+  const appDom = document.querySelector('#app');
+  // 跳转地址
+  const url = 'https://smartsignature.io';
+  // 接口地址
+  const baseUrl = 'https://api.smartsignature.io';
+  // 默认图片 logo
+  const logoImg = '../img/logo.png';
+  // 数据
+  let urlSearchData = {};
 
-    // axios 设置
-    const axiosApi = axios.create({
-      baseURL: baseUrl,
-      timeout: 30000,
+  // axios 设置
+  // eslint-disable-next-line no-undef
+  const axiosApi = axios.create({
+    baseURL: baseUrl,
+    timeout: 30000,
+  });
+
+  // 提取内容 删除多余的标签
+  const regRemoveContent = (str) => {
+    // 去除空格
+    const strTrim = str => str.replace(/\s+/g, '');
+    // 去除标签
+    const regRemoveTag = str => str.replace(/<[^>]+>/ig, '');
+    // 去除markdown img
+    const regRemoveMarkdownImg = str => str.replace(/!\[.*?\]\((.*?)\)/ig, '');
+    // 去除 markdown 标签
+    const regRemoveMarkdownTag = str => str.replace(/[\\\`\*\_\[\]\#\+\-\!\>]/ig, '');
+
+    const regRemoveTagResult = regRemoveTag(str);
+    const regRemoveMarkdownImgResult = regRemoveMarkdownImg(regRemoveTagResult);
+    const regRemoveMarkdownTagResult = regRemoveMarkdownTag(regRemoveMarkdownImgResult);
+    return strTrim(regRemoveMarkdownTagResult);
+  };
+
+  // url 参数解析
+  const urlSearchDecodeURIComponent = (arr) => {
+    const data = {};
+    arr.forEach((i) => {
+      const arrData = i.split('=');
+      data[arrData[0]] = decodeURIComponent(arrData[1]);
     });
+    return data;
+  };
+  urlSearchData = urlSearchDecodeURIComponent(urlSearch);
+  if (urlSearchData.content) urlSearchData.content = regRemoveContent(urlSearchData.content);
 
-    // 提取内容 删除多余的标签
-    const regRemoveContent = str => {
-      // 去除空格
-      const strTrim = str => str.replace(/\s+/g, '');
-      // 去除标签
-      const regRemoveTag = str => str.replace(/<[^>]+>/ig, '');
-      // 去除markdown img
-      const regRemoveMarkdownImg = str => str.replace(/!\[.*?\]\((.*?)\)/ig, '');
-      // 去除 markdown 标签
-      const regRemoveMarkdownTag = str => str.replace(/[\\\`\*\_\[\]\#\+\-\!\>]/ig, '');
-
-      const regRemoveTagResult = regRemoveTag(str)
-      const regRemoveMarkdownImgResult = regRemoveMarkdownImg(regRemoveTagResult)
-      const regRemoveMarkdownTagResult = regRemoveMarkdownTag(regRemoveMarkdownImgResult)
-      return strTrim(regRemoveMarkdownTagResult)
-    }
-
-    // url 参数解析
-    const urlSearchDecodeURIComponent = arr => {
-      let data = {}
-      arr.forEach(i => {
-        let arrData = i.split('=')
-        data[arrData[0]] = decodeURIComponent(arrData[1])
-      });
-      return data
-    }
-    urlSearchData = urlSearchDecodeURIComponent(urlSearch)
-    if (urlSearchData.content) urlSearchData.content = regRemoveContent(urlSearchData.content)
-
-    // 设置内容
-    const setAppDom = ({title, content, img, ups, read, username}) => {
-      let appDomStr = `
+  // 设置内容
+  const setAppDom = ({
+    title, content, img, ups, read, username,
+  }) => {
+    const appDomStr = `
       <div class="container">
         <div class="widget">
           <img class="logo" src="${logoImg}" alt="logo" />
@@ -64,71 +68,80 @@
             <span><img src="./img/ups.svg" alt="ups" />${ups || 0}</span>
           </di>
         </div>
-      </div>`
-      appDom.innerHTML = appDomStr
-      // show line 3
-      $clamp(document.querySelector('.widget-des'), {clamp: 3});
+      </div>`;
+    appDom.innerHTML = appDomStr;
+    // show line 3
+    // eslint-disable-next-line no-undef
+    $clamp(document.querySelector('.widget-des'), { clamp: 3 });
 
-      // 页面跳转
-      const titleClick = () => {
-       let jumpPageDom = document.querySelectorAll('.jumpPage')
-       jumpPageDom.forEach((i, index) => {
+    // 页面跳转
+    const titleClick = () => {
+      const jumpPageDom = document.querySelectorAll('.jumpPage');
+      jumpPageDom.forEach((i, index) => {
         jumpPageDom[index].addEventListener('click', () => {
-          let invite = urlSearchData.invite ? `?invite=${urlSearchData.invite}` : ''
-          window.open(`${url}/article/${urlSearchData.id || ''}${invite}`)  
-        }, false)
-       })
-      }
-      titleClick()
-    }
+          const invite = urlSearchData.invite ? `?invite=${urlSearchData.invite}` : '';
+          window.open(`${url}/article/${urlSearchData.id || ''}${invite}`);
+        }, false);
+      });
+    };
+    titleClick();
+  };
 
-    // 获取内容
-    const getArticleContent = hash => {
-      let url = `/ipfs/catJSON/${hash}`
-      axiosApi.get(url)
-        .then((res) => {
-          if (res.status === 200 && res.data.code === 200) {
-            let {data} = res.data
+  // 获取内容
+  const getArticleContent = (hash) => {
+    const url = `/ipfs/catJSON/${hash}`;
+    axiosApi.get(url)
+      .then((res) => {
+        if (res.status === 200 && res.data.code === 200) {
+          const { data } = res.data;
 
-            urlSearchData.content = regRemoveContent(data.content)
-            let {title,content, img,ups,read,username} = urlSearchData
-            setAppDom({title,content, img,ups,read, username})
-          } else {
-            console.error('请求失败')
-            setAppDom({})
-          }
-        })
-        .catch((err) => {
-          console.log(err)
-            setAppDom({})
-        })
-    }
+          urlSearchData.content = regRemoveContent(data.content);
+          const {
+            title, content, img, ups, read, username,
+          } = urlSearchData;
+          setAppDom({
+            title, content, img, ups, read, username,
+          });
+        } else {
+          console.error('请求失败');
+          setAppDom({});
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        setAppDom({});
+      });
+  };
 
-    // 通过id获取信息
-    const getInfobyId = id => {
-      let url = `/p/${id}`
-      axiosApi.get(url)
-        .then((response) => {
-          if (response.status === 200 && response.data.code === 0) {
-            let {data} = response.data
-            urlSearchData.title = data.title
-            urlSearchData.ups = data.ups
-            urlSearchData.read = data.read
-            urlSearchData.username = data.nickname || data.username
-            urlSearchData.img = data.cover ? baseUrl+ '/image/' +data.cover : logoImg
-            let {title,content, img,ups,read, username} = urlSearchData
-            setAppDom({title,content, img,ups,read, username})
-            // 没有传递内容请求接口获取
-            if (!urlSearchData.content) getArticleContent(data.hash)
-          } else {
-            console.error('请求失败')
-            setAppDom({})
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-            setAppDom({})
-        })
-    }
-    getInfobyId(urlSearchData.id)
-}())
+  // 通过id获取信息
+  const getInfobyId = (id) => {
+    const url = `/p/${id}`;
+    axiosApi.get(url)
+      .then((response) => {
+        if (response.status === 200 && response.data.code === 0) {
+          const { data } = response.data;
+          urlSearchData.title = data.title;
+          urlSearchData.ups = data.ups;
+          urlSearchData.read = data.read;
+          urlSearchData.username = data.nickname || data.username;
+          urlSearchData.img = data.cover ? `${baseUrl}/image/${data.cover}` : logoImg;
+          const {
+            title, content, img, ups, read, username,
+          } = urlSearchData;
+          setAppDom({
+            title, content, img, ups, read, username,
+          });
+          // 没有传递内容请求接口获取
+          if (!urlSearchData.content) getArticleContent(data.hash);
+        } else {
+          console.error('请求失败');
+          setAppDom({});
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        setAppDom({});
+      });
+  };
+  getInfobyId(urlSearchData.id);
+}());
