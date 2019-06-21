8156514d95665a3af6323ef11f4aae1e6c1fdca3
zxplus <shellteo@163.com>
2019-06-21 12:04:01 +0800

    update sidebar

--- src/views/Home/Sidebar.vue
+++ src/views/Home/Sidebar.vue
@@ -1,131 +1,186 @@
 <template>
   <van-popup v-model="sidebarShow" position="left" class="sidebar">
     <div class="container">
-      <div class="account">
-        <div class="avatar-container">
-          <img :src="account.avatar" alt="avatar" class="avatar">
-          <img src="@/assets/newimg/setting.svg" alt="setting" class="setting">
+      <template v-if="isLogined">
+        <div class="account">
+          <div class="avatar-container">
+            <img
+              :src="avatar"
+              alt="avatar"
+              class="avatar"
+              @click="$router.push({ name: 'UserEdit', params: { id } })"
+            />
+            <img
+              src="@/assets/newimg/setting.svg"
+              alt="setting"
+              class="setting"
+              @click="$router.push({ name: 'Help' })"
+            />
+          </div>
+          <p class="account-name">{{ name }}</p>
+          <p class="wallet-info">{{ displayBalanceSymbol }}钱包余额：{{ displayBalance }}</p>
         </div>
-        <p class="account-name">{{account.name}}</p>
-        <p class="wallet-info">EOS钱包余额：{{account.balance}}</p>
-      </div>
-      <div class="follow-info">
-        <div class="follow">
-          <p>864</p>
-          <p>关注</p>
+        <div class="follow-info">
+          <div class="follow">
+            <p>{{ follows }}</p>
+            <p>关注</p>
+          </div>
+          <div class="fans">
+            <p>{{ fans }}</p>
+            <p>粉丝</p>
+          </div>
         </div>
-        <div class="fans">
-          <p>121</p>
-          <p>粉丝</p>
+        <div class="cell-container">
+          <div class="cell" @click="$router.push({ name: 'Asset', params: { id } })">
+            <div class="cell-left">
+              <img src="@/assets/newimg/shouye-zhanghu.svg" alt="home" class="left-img" />
+              <span class="left-text">我的账户</span>
+            </div>
+            <div class="cell-right">
+              <span>已绑定{{ stats.accounts }}个账户</span>
+            </div>
+          </div>
         </div>
-      </div>
+        <div class="cell-container">
+          <div class="cell" @click="$router.push({ name: 'Original', params: { id } })">
+            <div class="cell-left">
+              <img src="@/assets/newimg/yuanchuang.svg" alt="article" class="left-img" />
+              <span class="left-text">原创文章</span>
+            </div>
+            <div class="cell-right">
+              <span>{{ stats.articles }}篇</span>
+            </div>
+          </div>
+          <div class="cell" @click="$router.push({ name: 'Reward', params: { id } })">
+            <div class="cell-left">
+              <img src="@/assets/newimg/zanshang.svg" alt="article" class="left-img" />
+              <span class="left-text">赞赏文章</span>
+            </div>
+            <div class="cell-right">
+              <span>{{ stats.supports }}篇</span>
+            </div>
+          </div>
+          <div class="cell" @click="$router.push({ name: 'DraftBox', params: { id } })">
+            <div class="cell-left">
+              <img src="@/assets/newimg/caogaoxiang.svg" alt="article" class="left-img" />
+              <span class="left-text">草稿箱</span>
+            </div>
+            <div class="cell-right">
+              <span>{{ stats.drafts }}篇</span>
+            </div>
+          </div>
+        </div>
+        <div class="cell-container">
+          <div class="cell" @click="$router.push({ name: 'BuyHistory' })">
+            <div class="cell-left">
+              <img src="@/assets/newimg/goumaijilu.svg" alt="article" class="left-img" />
+              <span class="left-text">购买记录</span>
+            </div>
+            <div class="cell-right"><span></span></div>
+          </div>
+        </div>
+      </template>
+      <template v-else>
+        <div class="login-btn" href="javascript:;" @click="showModal = true">立即登录</div>
+      </template>
       <div class="cell-container">
-        <div class="cell">
+        <div class="cell" @click="$router.push({ name: 'About' })">
           <div class="cell-left">
-            <img src="@/assets/newimg/shouye-zhanghu.svg" alt="home" class="left-img">
-            <span class="left-text">我的账户</span>
-          </div>
-          <div class="cell-right"><span>已绑定2个账户</span></div>
-        </div>
-      </div>
-      <div class="cell-container">
-        <div class="cell">
-          <div class="cell-left">
-            <img src="@/assets/newimg/yuanchuang.svg" alt="article" class="left-img">
-            <span class="left-text">原创文章</span>
-          </div>
-          <div class="cell-right"><span>100篇</span></div>
-        </div>
-        <div class="cell">
-          <div class="cell-left">
-            <img src="@/assets/newimg/zanshang.svg" alt="article" class="left-img">
-            <span class="left-text">赞赏文章</span>
-          </div>
-          <div class="cell-right"><span>1篇</span></div>
-        </div>
-        <div class="cell">
-          <div class="cell-left">
-            <img src="@/assets/newimg/caogaoxiang.svg" alt="article" class="left-img">
-            <span class="left-text">草稿箱</span>
-          </div>
-          <div class="cell-right"><span>1篇</span></div>
-        </div>
-      </div>
-      <div class="cell-container">
-        <div class="cell active">
-          <div class="cell-left">
-            <img src="@/assets/newimg/goumaijilu.svg" alt="article" class="left-img">
-            <span class="left-text">购买记录</span>
-          </div>
-          <div class="cell-right"><span></span></div>
-        </div>
-      </div>
-      <div class="cell-container">
-        <div class="cell">
-          <div class="cell-left">
-            <img src="@/assets/newimg/gonglue.svg" alt="article" class="left-img">
+            <img src="@/assets/newimg/gonglue.svg" alt="article" class="left-img" />
             <span class="left-text">投资攻略</span>
           </div>
           <div class="cell-right"><span></span></div>
         </div>
-        <div class="cell">
-          <div class="cell-left">
-            <img src="@/assets/newimg/telegram.svg" alt="article" class="left-img">
-            <span class="left-text">加入电报</span>
+        <a href="https://t.me/smartsignature_io" target="_blank">
+          <div class="cell">
+            <div class="cell-left">
+              <img src="@/assets/newimg/telegram.svg" alt="article" class="left-img" />
+              <span class="left-text">加入电报</span>
+            </div>
+            <div class="cell-right"><span></span></div>
           </div>
-          <div class="cell-right"><span></span></div>
-        </div>
+        </a>
       </div>
     </div>
+    <BaseModalForSignIn :show-modal="showModal" @changeInfo="changeInfo" />
   </van-popup>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
+
 export default {
-  name: 'Sidebar',
+  name: "Sidebar",
   props: {
     value: {
       type: Boolean,
-      default: false,
-    },
+      default: false
+    }
   },
   data() {
     return {
+      showModal: false,
       sidebarShow: false,
-      account: {
-        avatar: 'https://www.rollingstone.com/wp-content/uploads/2018/06/rs-196217-169526716.jpg',
-        name: 'Penny',
-        balance: 1,
-      },
+      balance: 1,
+      id: null,
       followed: false,
       follows: 0,
       fans: 0,
-      name: '',
-      email: '',
-      avatar: '',
-      introduction: '',
+      name: "",
+      email: "",
+      avatar: "",
+      introduction: "",
       stats: {
         accounts: 0,
         articles: 0,
         supports: 0,
-        drafts: 0,
-      },
+        drafts: 0
+      }
     };
+  },
+  computed: {
+    ...mapGetters(["currentUserInfo", "displayName", "isLogined", "isMe"]),
+    displayBalance() {
+      const { balance } = this.currentUserInfo;
+      return balance ? balance.slice(0, -4) : "";
+    },
+    displayBalanceSymbol() {
+      const { balance } = this.currentUserInfo;
+      return balance ? balance.slice(-4) : "";
+    }
   },
   watch: {
     value(val) {
       this.sidebarShow = val;
     },
     sidebarShow(val) {
-      this.$emit('input', val);
+      this.$emit("input", val);
     },
+    isLogined(newState) {
+      if (newState) this.refreshUser();
+    }
+  },
+  created() {
+    this.refreshUser();
+    this.id = this.currentUserInfo.id;
   },
   methods: {
     async refreshUser() {
-      const { isMe, id } = this;
+      const { isMe } = this;
+      const id = this.currentUserInfo.id;
       const setUser = ({
-        avatar, email, fans, follows, is_follow, nickname, introduction, username, accounts, articles, supports, drafts,
+        avatar,
+        email,
+        fans,
+        follows,
+        is_follow,
+        nickname,
+        introduction,
+        username,
+        accounts,
+        articles,
+        supports,
+        drafts
       }) => {
         this.email = email;
         this.fans = fans;
@@ -135,22 +190,27 @@ export default {
         this.name = nickname || username;
         this.setAvatarImage(avatar);
         this.stats = {
-          accounts, articles, supports, drafts,
+          accounts,
+          articles,
+          supports,
+          drafts
         };
       };
 
       const { getMyUserData, getUser } = this.$backendAPI;
-      const { data: { data } } = await (isMe(id) ? getMyUserData() : getUser({ id }));
+      const {
+        data: { data }
+      } = await (isMe(id) ? getMyUserData() : getUser({ id }));
       // console.debug(data);
       setUser(data);
     },
-  },
-  computed: {
-    ...mapGetters(['currentUserInfo', 'displayName', 'isLogined', 'isMe']),
-  },
-  created() {
-    this.refreshUser();
-  },
+    setAvatarImage(hash) {
+      if (hash) this.avatar = this.$backendAPI.getAvatarImage(hash);
+    },
+    changeInfo(status) {
+      this.showModal = status;
+    }
+  }
 };
 </script>
 
@@ -159,8 +219,20 @@ export default {
   height: 100%;
   width: 75%;
   max-width: 280px;
-  background-color: #F1F1F1;
+  background-color: #f1f1f1;
   color: #000000;
+  z-index: 1000!important;
+  .login-btn {
+    margin: 50px 30px;
+    background-color: #000;
+    color: #fff;
+    padding: 8px 20px;
+    border-radius: 3px;
+    font-size: 14px;
+    font-weight: 400;
+    letter-spacing: 1px;
+    min-width: 100px;
+  }
   .container {
     .account {
       padding: 20px;
@@ -198,7 +270,8 @@ export default {
       background-color: #ffffff;
       padding: 10px;
       box-sizing: border-box;
-      .follow, .fans {
+      .follow,
+      .fans {
         display: inline-block;
         width: 50%;
         color: #000000;
@@ -243,7 +316,7 @@ export default {
     }
     .cell-right {
       span {
-        color: #B2B2B2;
+        color: #b2b2b2;
         font-size: 12px;
         line-height: 17px;
       }
@@ -251,8 +324,13 @@ export default {
   }
   .cell.active {
     .left-text {
-      color: #1C9CFE;
+      color: #1c9cfe;
     }
   }
 }
+</style>
+<style>
+  .van-overlay {
+    z-index: 999!important;
+  }
 </style>
