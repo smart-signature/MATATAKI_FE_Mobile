508735e57b213662d8175b3c31760bf98a18b781
xiaotiandada <952822399@qq.com>
2019-06-28 17:04:57 +0800

    🔧fix 关注粉丝列表
    🔧fix 关注粉丝列表

--- src/views/User/FollowList/FollowList.vue
+++ src/views/User/FollowList/FollowList.vue
@@ -2,19 +2,19 @@
   <div class="mw draftbox">
     <BaseHeader :pageinfo="{ title: '列表', rightPage: 'home' }" />
     <Tabs :value="activeIndex">
-      <TabPane v-for="(item, index) in tabsData" :key="index" :label="item.label" >
+      <TabPane v-for="(item, index) in tabsData" :key="index" :label="item.label">
         <BasePull
           class="draftbox-list"
-          :loadingText = "loadingText"
+          :loading-text="loadingText"
           :params="item.params"
-          :apiUrl="item.apiUrl"
-          :activeIndex="activeIndex"
-          :needAccessToken="true"
-          :nowIndex="index"
-          :isObj="{ type: 'Object', key: 'list' }"
+          :api-url="item.apiUrl"
+          :active-index="activeIndex"
+          :need-access-token="true"
+          :now-index="index"
+          :is-obj="{ type: 'newObject', key: 'data', keys: 'list' }"
           @getListData="getListData"
-          >
-            <list :list="item" :ownerId="id" v-for="(item, index) in item.articles" :key="index"/>
+        >
+          <list v-for="(item, index) in item.articles" :key="index" :list="item" :owner-id="id" />
         </BasePull>
       </TabPane>
     </Tabs>
@@ -22,48 +22,47 @@
 </template>
 
 <script>
-import list from './list.vue';
+import list from "./list.vue";
 
 export default {
-  name: 'DeaftBox',
-  props: ['id', 'listtype'],
+  name: "DeaftBox",
   components: { list },
+  props: ["id", "listtype"],
   data() {
     return {
       tabsData: [
         {
-          label: '关注',
+          label: "关注",
           params: { uid: this.id },
-          apiUrl: 'followsList',
-          articles: [],
+          apiUrl: "followsList",
+          articles: []
         },
         {
-          label: '粉丝',
+          label: "粉丝",
           params: { uid: this.id },
-          apiUrl: 'fansList',
-          articles: [],
-        },
+          apiUrl: "fansList",
+          articles: []
+        }
       ],
       activeIndex: 0,
       activeIndexName: this.listtype,
       loadingText: {
-        nomore: '',
-        noresults: '没有关注或粉丝',
-      },
+        nomore: "",
+        noresults: "没有关注或粉丝"
+      }
     };
   },
-  computed: {
-  },
+  computed: {},
   created() {
-    if (this.activeIndexName === '关注') this.activeIndex = 0;
-    else if (this.activeIndexName === '粉丝') this.activeIndex = 1;
+    if (this.activeIndexName === "关注") this.activeIndex = 0;
+    else if (this.activeIndexName === "粉丝") this.activeIndex = 1;
     else this.activeIndex = 0;
   },
   methods: {
     getListData(res) {
       this.tabsData[res.index].articles = res.list;
-    },
-  },
+    }
+  }
 };
 </script>
 <style scoped>
