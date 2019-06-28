b34fdc895802e40ef0a4ac104c5e1d133c0852b8
xiaotiandada <952822399@qq.com>
2019-06-28 11:38:11 +0800

    🚀 登录面板中新增ONTO 钱包图标
    登录面板中新增ONTO 钱包图标

--- src/components/baseComponents/BaseModalForSignIn.vue
+++ src/components/baseComponents/BaseModalForSignIn.vue
@@ -1,161 +1,174 @@
 <template>
-  <Modal
-    v-model="showModaCopy"
-    footer-hide
-    @on-visible-change="change"
-    class-name="modalCenter">
-      <div class="info-content">
-        <p class="info-content-title">{{modalText.text}}</p>
+  <Modal v-model="showModaCopy" footer-hide class-name="modalCenter" @on-visible-change="change">
+    <div class="info-content">
+      <p class="info-content-title">{{ modalText.text }}</p>
 
-        <div class="modal-login">
-          <div class="modal-body" v-for="(item, index) in idProvider" :key="index">
-            <div class="modal-body-content">
-              <div class="modal-body-head">支持钱包</div>
-                <div class="modal-wallet">
-                  <a :href="itemWallet.href" target="_blank" v-for="(itemWallet, indexWallet) in item.wallet" :key="indexWallet">
-                    <img :src="itemWallet.url" :alt="itemWallet.alt" />
-                  </a>
-                </div>
-                <div class="modal-logo">
-                  <div class="modal-logo-button" :class="'active'+index" @click="walletLogin(item.type)">
-                    <img :src="item.url" :alt="item.title" />
-                    <span>{{item.title}}</span>
-                  </div>
-                </div>
-
+      <div class="modal-login">
+        <div v-for="(item, index) in idProvider" :key="index" class="modal-body">
+          <div class="modal-body-content">
+            <div class="modal-body-head">支持钱包</div>
+            <div class="modal-wallet">
+              <a
+                v-for="(itemWallet, indexWallet) in item.wallet"
+                :key="indexWallet"
+                :href="itemWallet.href"
+                target="_blank"
+              >
+                <img :src="itemWallet.url" :alt="itemWallet.alt" />
+              </a>
             </div>
-            <a class="modal-doc" target="_blank" :href="item.doc.href">{{item.doc.title}}</a>
-
+            <div class="modal-logo">
+              <div
+                class="modal-logo-button"
+                :class="'active' + index"
+                @click="walletLogin(item.type)"
+              >
+                <img :src="item.url" :alt="item.title" />
+                <span>{{ item.title }}</span>
+              </div>
+            </div>
           </div>
-
-        </div>
-        <Divider>或者</Divider>
-          <div class="modal-logo-button" :class="'active2'" @click="walletLogin('GitHub')"
-            style="text-align: center;"
-          >
-            <img :src="iconGithub" :alt="'iconGithub'" />
-          </div>
-        <div class="modal-loading" v-if="modalLoading">
-          <van-loading type="spinner" color="#1989fa" />
+          <a class="modal-doc" target="_blank" :href="item.doc.href">{{ item.doc.title }}</a>
         </div>
       </div>
+      <Divider>或者</Divider>
+      <div
+        class="modal-logo-button"
+        :class="'active2'"
+        style="text-align: center;"
+        @click="walletLogin('GitHub')"
+      >
+        <img :src="iconGithub" :alt="'iconGithub'" />
+      </div>
+      <div v-if="modalLoading" class="modal-loading">
+        <van-loading type="spinner" color="#1989fa" />
+      </div>
+    </div>
   </Modal>
 </template>
 
 <script>
-import { mapActions, mapMutations, mapState } from 'vuex';
-import iconEOS from '@/assets/img/icon_logo_eos.svg';
-import iconTokenpocket from '@/assets/img/icon_tokenpocket.svg';
-import iconMathwallet from '@/assets/img/icon_mathwallet.svg';
-import iconScatter from '@/assets/img/icon_scatter.svg';
-import iconONT from '@/assets/img/icon_logo_ont.svg';
-import iconCyano from '@/assets/img/icon_cyano.svg';
-import iconLeafwallet from '@/assets/img/icon_leafwallet.svg';
-import iconChallte from '@/assets/img/icon_challte.svg';
-import iconMeet from '@/assets/img/icon_meet.svg';
-import iconGithub from '@/assets/img/icon_logo_github.svg';
+import { mapActions, mapMutations, mapState } from "vuex";
+import iconEOS from "@/assets/img/icon_logo_eos.svg";
+import iconTokenpocket from "@/assets/img/icon_tokenpocket.svg";
+import iconMathwallet from "@/assets/img/icon_mathwallet.svg";
+import iconScatter from "@/assets/img/icon_scatter.svg";
+import iconONT from "@/assets/img/icon_logo_ont.svg";
+import iconCyano from "@/assets/img/icon_cyano.svg";
+import iconLeafwallet from "@/assets/img/icon_leafwallet.svg";
+import iconChallte from "@/assets/img/icon_challte.svg";
+import iconMeet from "@/assets/img/icon_meet.svg";
+import iconGithub from "@/assets/img/icon_logo_github.svg";
+import iconOnto from "@/assets/img/icon_logo_onto.svg";
 
 export default {
-  name: 'BaseModalForSignIn',
+  name: "BaseModalForSignIn",
   props: {
     showModal: {
       type: Boolean,
-      default: false,
-    },
+      default: false
+    }
   },
   watch: {
     showModal(newVal) {
       this.showModaCopy = newVal;
-    },
+    }
   },
   computed: {
-    ...mapState(['userConfig']),
+    ...mapState(["userConfig"])
   },
   data() {
     return {
       showModaCopy: this.showModal,
       modalLoading: false,
       modalText: {
-        text: '选择授权方式',
+        text: "选择授权方式"
       },
       iconGithub,
       idProvider: [
         {
           url: iconEOS,
-          title: 'EOS登录',
-          type: 'EOS',
+          title: "EOS登录",
+          type: "EOS",
           wallet: [
             {
               url: iconTokenpocket,
-              href: 'https://www.tokenpocket.pro/',
-              alt: 'https://www.tokenpocket.pro/',
+              href: "https://www.tokenpocket.pro/",
+              alt: "https://www.tokenpocket.pro/"
             },
             {
               url: iconScatter,
-              href: 'https://get-scatter.com/',
-              alt: 'https://get-scatter.com/',
+              href: "https://get-scatter.com/",
+              alt: "https://get-scatter.com/"
             },
             {
               url: iconChallte,
-              href: 'http://eblock.io/',
-              alt: 'http://eblock.io/',
+              href: "http://eblock.io/",
+              alt: "http://eblock.io/"
             },
             {
               url: iconMathwallet,
-              href: 'http://www.medishares.org/',
-              alt: 'http://www.medishares.org/',
+              href: "http://www.medishares.org/",
+              alt: "http://www.medishares.org/"
             },
             {
               url: iconLeafwallet,
-              href: 'https://www.leafwallet.io/',
-              alt: 'https://www.leafwallet.io/',
+              href: "https://www.leafwallet.io/",
+              alt: "https://www.leafwallet.io/"
             },
             {
               url: iconMeet,
-              href: 'https://meet.one/',
-              alt: 'https://meet.one/',
-            },
+              href: "https://meet.one/",
+              alt: "https://meet.one/"
+            }
           ],
           doc: {
-            title: '《如何使用EOS登录》',
-            href: 'https://smartsignature.io/article/515',
-          },
+            title: "《如何使用EOS登录》",
+            href: "https://smartsignature.io/article/515"
+          }
         },
         {
           url: iconONT,
-          title: 'ONT登录',
-          type: 'ONT',
+          title: "ONT登录",
+          type: "ONT",
           wallet: [
             {
-              url: iconMathwallet,
-              href: 'http://www.medishares.org/',
-              alt: 'http://www.medishares.org/',
+              url: iconOnto,
+              href: "https://onto.app/",
+              alt: "https://onto.app/"
             },
             {
               url: iconCyano,
-              href: 'https://chrome.google.com/webstore/detail/cyano-wallet/dkdedlpgdmmkkfjabffeganieamfklkm',
-              alt: 'https://chrome.google.com/webstore/detail/cyano-wallet/dkdedlpgdmmkkfjabffeganieamfklkm',
+              href:
+                "https://chrome.google.com/webstore/detail/cyano-wallet/dkdedlpgdmmkkfjabffeganieamfklkm",
+              alt:
+                "https://chrome.google.com/webstore/detail/cyano-wallet/dkdedlpgdmmkkfjabffeganieamfklkm"
             },
+            {
+              url: iconMathwallet,
+              href: "http://www.medishares.org/",
+              alt: "http://www.medishares.org/"
+            }
           ],
           doc: {
-            title: '《如何使用ONT登录》',
-            href: 'https://smartsignature.io/article/516',
-          },
-        },
-      ],
+            title: "《如何使用ONT登录》",
+            href: "https://smartsignature.io/article/516"
+          }
+        }
+      ]
     };
   },
   methods: {
-    ...mapActions(['signIn']),
+    ...mapActions(["signIn"]),
     change(status) {
       if (this.modalLoading) this.modalLoading = false;
       this.showModaCopy = status;
-      this.$emit('changeInfo', status);
+      this.$emit("changeInfo", status);
     },
     async walletLogin(type) {
       this.modalLoading = true;
-      if (type === 'GitHub') {
-        this.$router.push({ name: 'Login' });
+      if (type === "GitHub") {
+        this.$router.push({ name: "Login" });
         return;
       }
       await this.signInx(type);
@@ -172,20 +185,19 @@ export default {
           console.log(err);
           this.$toast.fail({
             duration: 1000,
-            message: '登陆失败',
+            message: "登陆失败"
           });
         }
       }
-    },
-  },
+    }
+  }
 };
 </script>
-
 
 <style lang="less" scoped>
 .info-content {
   margin: 0 30px;
-  transition: all .3s;
+  transition: all 0.3s;
   &-title {
     text-align: center;
     font-size: 18px;
@@ -204,7 +216,7 @@ export default {
     flex: 0 0 50%;
     text-align: center;
     margin: 0 14px;
-    transition: all .3s;
+    transition: all 0.3s;
     &-content {
       background-color: #f0f0f0;
       border-radius: 4px;
@@ -218,20 +230,20 @@ export default {
 
     &-head {
       width: 100%;
-      background: #E6E6E6;
+      background: #e6e6e6;
       padding: 7px 0;
       margin: 0 0 10px 0;
       font-size: 14px;
       font-weight: 400;
-      color: rgba(0,0,0,.7);
+      color: rgba(0, 0, 0, 0.7);
     }
 
     .modal-logo {
-        flex: 1;
-        display: flex;
-        align-items: flex-end;
-        width: 100%;
-        padding: 0 20px;
+      flex: 1;
+      display: flex;
+      align-items: flex-end;
+      width: 100%;
+      padding: 0 20px;
       &-button {
         text-align: center;
         color: #fff;
@@ -248,22 +260,22 @@ export default {
           background: #444;
         }
         &.active1 {
-          background: #4D9AFD;
+          background: #4d9afd;
         }
-      img {
-        height: 16px;
-        padding: 0;
-        margin: 8px 8px 8px 0;
-        cursor: pointer;
-        box-sizing: border-box;
+        img {
+          height: 16px;
+          padding: 0;
+          margin: 8px 8px 8px 0;
+          cursor: pointer;
+          box-sizing: border-box;
+        }
+        span {
+          font-size: 14px;
+          font-weight: 400;
+          padding: 0;
+          margin: 0;
+        }
       }
-      span {
-        font-size: 14px;
-        font-weight: 400;
-        padding: 0;
-        margin: 0;
-      }
-    }
     }
     .modal-wallet {
       background: rgba(237, 237, 237, 0.75);
@@ -293,9 +305,9 @@ export default {
       }
     }
     .modal-doc {
-      font-size:14px;
-      font-weight:400;
-      color:rgba(0,118,255,1);
+      font-size: 14px;
+      font-weight: 400;
+      color: rgba(0, 118, 255, 1);
       margin: 34px 0 28px 0;
       display: inline-block;
     }
